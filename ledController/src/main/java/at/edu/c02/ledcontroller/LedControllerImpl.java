3c509785411dda248160c3bf553b42cd31918196
maximilianhofer <52204365@edu.campus02.at>
2025-01-25 14:04:27 +0100

    Hotfix TurnOffAll

--- ledController/src/main/java/at/edu/c02/ledcontroller/LedControllerImpl.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/LedControllerImpl.java
@@ -79,6 +79,7 @@ public class LedControllerImpl implements LedController {
                 JSONObject temp = tempArray.getJSONObject(j);
                 int id = temp.getInt("id");
                 apiService.putLight(id, "#FF5733", false);
+                return;
             }
         }
     }
